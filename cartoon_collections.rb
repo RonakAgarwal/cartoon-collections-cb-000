5d6acae8cd4b99d06a4f86573bd074f089559123
RonakAgarwal <agarwalronak02@gmail.com>
2018-07-14 14:46:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0
+  while < array.length
+    puts "#{i + 1}. #{array[i]}"
+    i += 1
+  end
+  
 end
 
 def summon_captain_planet# code an argument here
